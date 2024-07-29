d78a8c94f9c633fcdde95521c196f74d7c7b2461
tourist1642 <tourist1642@gmail.com>
2024-07-29 22:40:44 +0530

    Add all project files

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import Temperature from "./componenets/Temperature";
+import Highlights from "./componenets/Highlights";
+
+function App() {
+  const [city, setCity] = useState("New Delhi");
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=0eb2f9e6dd554873aa7120429230511&q=${city}&aqi=no;`;
+
+    fetch(apiUrl)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Could not get data");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setWeatherData(data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, [city]);
+  return (
+    <div className="bg-slate-800 h-screen flex justify-center  items-start">
+      <div className="w-1/5 h-1/3 mt-40">
+        {weatherData && (
+          <Temperature
+            setCity={setCity}
+            stats={{
+              temp: weatherData.current.temp_c,
+              condition: weatherData.current.condition.text,
+              isDay: weatherData.current.is_day,
+              location: weatherData.location.name,
+              time: weatherData.location.localtime,
+            }}
+          />
+        )}
+      </div>
+      <div className="w-1/3 h-1/3 mt-40 p-10 grid grid-cols-2 gap-6">
+        <h1 className="text-slate-200 text-2xl col-span-2">
+          Today's Highlights
+        </h1>
+        {weatherData && (
+          <>
+            <Highlights
+              stats={{
+                title: "Wind Status",
+                value: weatherData.current.wind_mph,
+                unit: "mph",
+                direction: weatherData.current.wind_dir,
+              }}
+            />
+            <Highlights
+              stats={{
+                title: "Humidity",
+                value: weatherData.current.humidity,
+                unit: "%",
+              }}
+            />
+            <Highlights
+              stats={{
+                title: "Visibility",
+                value: weatherData.current.vis_miles,
+                unit: "miles",
+              }}
+            />
+            <Highlights
+              stats={{
+                title: "Air Pressure",
+                value: weatherData.current.pressure_mb,
+                unit: "mb",
+              }}
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
